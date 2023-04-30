b3b4a77738ce5ceefec7f1fb0efe6d304519a475
msmhmorsi <msmhmorsi@connect.ust.hk>
2023-05-01 01:27:31 +0800

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import app_logo from './app_logo.png';
+import preview from './preview.png';
+
+export {
+  download,
+  logo,
+  app_logo,
+  preview,
+};
---
+++ client/utils/index.js
@@ -0,0 +1,10 @@
+import { surpriseMePrompts } from "../src/constants";
+
+export function getRandomPrompt(prompt) {
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length) ; 
+    const randomPrompt = surpriseMePrompts[randomIndex];
+
+    if(randomPrompt === prompt)return getRandomPrompt() ; 
+
+    return randomPrompt ;
+}
